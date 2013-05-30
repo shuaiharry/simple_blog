b0d84340f7402bd71202fa21c200fd6365e7e213
Duhee Lee <doo2@wafflestudio.com>
2013-05-30 15:39:50 +0900

    add attrs in post & comment model

--- db/migrate/20130530063751_create_posts.rb
+++ db/migrate/20130530063751_create_posts.rb
@@ -1,6 +1,8 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
+      t.string      :username
+      t.string      :content
 
       t.timestamps
     end
--- db/migrate/20130530063759_create_comments.rb
+++ db/migrate/20130530063759_create_comments.rb
@@ -1,6 +1,9 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
+      t.integer   :post_id,   :null => false
+      t.string    :username
+      t.string    :content
 
       t.timestamps
     end
